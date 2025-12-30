ee07c085030c4dccea28a56f188285a6063cc9bd
amorales-00 <[alemperez24@gmail.com]>
2025-12-30 11:18:47 -0500

    another update

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True, db_column='ID')
+
+    name = models.CharField(max_length=255, db_column='Name')
+    no_of_guests = models.PositiveIntegerField(
+        validators=[MinValueValidator(1)],
+        db_column='No_of_guests',
+    )
+    booking_date = models.DateTimeField(db_column='BookingDate')
+
+    class Meta:
+        db_table = 'Booking'
+
+    def __str__(self) -> str:
+        return f'{self.name} ({self.no_of_guests}) @ {self.booking_date}'
+
+
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True, db_column='ID')
+
+    title = models.CharField(max_length=255, db_column='Title')
+    price = models.DecimalField(max_digits=10, decimal_places=2, db_column='Price')
+    inventory = models.PositiveIntegerField(
+        validators=[MinValueValidator(0)],
+        db_column='Inventory',
+    )
+
+    class Meta:
+        db_table = 'Menu'
+
+    def __str__(self) -> str:
+        return f'{self.title} - ${self.price}'
